7ae7160b304a9675b114a771d4e9b66f5f794f35
VadymMakohon <errorks7@gmail.com>
2024-07-27 21:53:19 +0200

    added move function

--- flappy_bird.py
+++ flappy_bird.py
@@ -47,3 +47,29 @@ def jump(self):
         self.vel = -10.5
         self.tick_count = 0
         self.height = self.y
+
+def move(self):
+        """
+        make the bird move
+        :return: None
+        """
+        self.tick_count += 1
+
+        # for downward acceleration
+        displacement = self.vel*(self.tick_count) + 0.5*(3)*(self.tick_count)**2  # calculate displacement
+
+        # terminal velocity
+        if displacement >= 16:
+            displacement = (displacement/abs(displacement)) * 16
+
+        if displacement < 0:
+            displacement -= 2
+
+        self.y = self.y + displacement
+
+        if displacement < 0 or self.y < self.height + 50:  # tilt up
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:  # tilt down
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
