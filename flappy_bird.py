f4b7f59e6e7377462a5d21aa0aa9b44ee3238afd
VadymMakohon <errorks7@gmail.com>
2024-07-27 21:07:09 +0200

    setting up the game with basic paramaters and imgs

---
+++ flappy_bird.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+import time
+import os
+import neat
+
+WIN_WIDTH = 600
+WIN_HIGHT = 800
+
+pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")).convert_alpha())
+bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs","bg.png")).convert_alpha(), (600, 900))
+bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird" + str(x) + ".png"))) for x in range(1,4)]
+base_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")).convert_alpha())
