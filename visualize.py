7ff60d45dab4d5a30947d1b64b0165044beccf2e
VadymMakohon <errorks7@gmail.com>
2024-07-28 11:17:48 +0200

    updated code

--- visualize.py
+++ visualize.py
@@ -88,110 +88,110 @@ def plot_spikes(spikes, view=False, filename=None, title=None):
     return fig
 
 
-# def plot_species(statistics, view=False, filename='speciation.svg'):
-#     """ Visualizes speciation throughout evolution. """
-#     if plt is None:
-#         warnings.warn("This display is not available due to a missing optional dependency (matplotlib)")
-#         return
+def plot_species(statistics, view=False, filename='speciation.svg'):
+    """ Visualizes speciation throughout evolution. """
+    if plt is None:
+        warnings.warn("This display is not available due to a missing optional dependency (matplotlib)")
+        return
 
-#     species_sizes = statistics.get_species_sizes()
-#     num_generations = len(species_sizes)
-#     curves = np.array(species_sizes).T
+    species_sizes = statistics.get_species_sizes()
+    num_generations = len(species_sizes)
+    curves = np.array(species_sizes).T
 
-#     fig, ax = plt.subplots()
-#     ax.stackplot(range(num_generations), *curves)
+    fig, ax = plt.subplots()
+    ax.stackplot(range(num_generations), *curves)
 
-#     plt.title("Speciation")
-#     plt.ylabel("Size per Species")
-#     plt.xlabel("Generations")
+    plt.title("Speciation")
+    plt.ylabel("Size per Species")
+    plt.xlabel("Generations")
 
-#     plt.savefig(filename)
+    plt.savefig(filename)
 
-#     if view:
-#         plt.show()
+    if view:
+        plt.show()
 
-#     plt.close()
+    plt.close()
 
 
-# def draw_net(config, genome, view=False, filename=None, node_names=None, show_disabled=True, prune_unused=False,
-#              node_colors=None, fmt='svg'):
-#     """ Receives a genome and draws a neural network with arbitrary topology. """
-#     # Attributes for network nodes.
-#     if graphviz is None:
-#         warnings.warn("This display is not available due to a missing optional dependency (graphviz)")
-#         return
+def draw_net(config, genome, view=False, filename=None, node_names=None, show_disabled=True, prune_unused=False,
+             node_colors=None, fmt='svg'):
+    """ Receives a genome and draws a neural network with arbitrary topology. """
+    # Attributes for network nodes.
+    if graphviz is None:
+        warnings.warn("This display is not available due to a missing optional dependency (graphviz)")
+        return
 
-#     if node_names is None:
-#         node_names = {}
+    if node_names is None:
+        node_names = {}
 
-#     assert type(node_names) is dict
+    assert type(node_names) is dict
 
-#     if node_colors is None:
-#         node_colors = {}
+    if node_colors is None:
+        node_colors = {}
 
-#     assert type(node_colors) is dict
+    assert type(node_colors) is dict
 
-#     node_attrs = {
-#         'shape': 'circle',
-#         'fontsize': '9',
-#         'height': '0.2',
-#         'width': '0.2'}
+    node_attrs = {
+        'shape': 'circle',
+        'fontsize': '9',
+        'height': '0.2',
+        'width': '0.2'}
 
-#     dot = graphviz.Digraph(format=fmt, node_attr=node_attrs)
+    dot = graphviz.Digraph(format=fmt, node_attr=node_attrs)
 
-#     inputs = set()
-#     for k in config.genome_config.input_keys:
-#         inputs.add(k)
-#         name = node_names.get(k, str(k))
-#         input_attrs = {'style': 'filled', 'shape': 'box', 'fillcolor': node_colors.get(k, 'lightgray')}
-#         dot.node(name, _attributes=input_attrs)
+    inputs = set()
+    for k in config.genome_config.input_keys:
+        inputs.add(k)
+        name = node_names.get(k, str(k))
+        input_attrs = {'style': 'filled', 'shape': 'box', 'fillcolor': node_colors.get(k, 'lightgray')}
+        dot.node(name, _attributes=input_attrs)
 
-#     outputs = set()
-#     for k in config.genome_config.output_keys:
-#         outputs.add(k)
-#         name = node_names.get(k, str(k))
-#         node_attrs = {'style': 'filled', 'fillcolor': node_colors.get(k, 'lightblue')}
+    outputs = set()
+    for k in config.genome_config.output_keys:
+        outputs.add(k)
+        name = node_names.get(k, str(k))
+        node_attrs = {'style': 'filled', 'fillcolor': node_colors.get(k, 'lightblue')}
 
-#         dot.node(name, _attributes=node_attrs)
+        dot.node(name, _attributes=node_attrs)
 
-#     if prune_unused:
-#         connections = set()
-#         for cg in genome.connections.values():
-#             if cg.enabled or show_disabled:
-#                 connections.add((cg.in_node_id, cg.out_node_id))
+    if prune_unused:
+        connections = set()
+        for cg in genome.connections.values():
+            if cg.enabled or show_disabled:
+                connections.add((cg.in_node_id, cg.out_node_id))
 
-#         used_nodes = copy.copy(outputs)
-#         pending = copy.copy(outputs)
-#         while pending:
-#             new_pending = set()
-#             for a, b in connections:
-#                 if b in pending and a not in used_nodes:
-#                     new_pending.add(a)
-#                     used_nodes.add(a)
-#             pending = new_pending
-#     else:
-#         used_nodes = set(genome.nodes.keys())
+        used_nodes = copy.copy(outputs)
+        pending = copy.copy(outputs)
+        while pending:
+            new_pending = set()
+            for a, b in connections:
+                if b in pending and a not in used_nodes:
+                    new_pending.add(a)
+                    used_nodes.add(a)
+            pending = new_pending
+    else:
+        used_nodes = set(genome.nodes.keys())
 
-#     for n in used_nodes:
-#         if n in inputs or n in outputs:
-#             continue
+    for n in used_nodes:
+        if n in inputs or n in outputs:
+            continue
 
-#         attrs = {'style': 'filled',
-#                  'fillcolor': node_colors.get(n, 'white')}
-#         dot.node(str(n), _attributes=attrs)
+        attrs = {'style': 'filled',
+                 'fillcolor': node_colors.get(n, 'white')}
+        dot.node(str(n), _attributes=attrs)
 
-#     for cg in genome.connections.values():
-#         if cg.enabled or show_disabled:
-#             #if cg.input not in used_nodes or cg.output not in used_nodes:
-#             #    continue
-#             input, output = cg.key
-#             a = node_names.get(input, str(input))
-#             b = node_names.get(output, str(output))
-#             style = 'solid' if cg.enabled else 'dotted'
-#             color = 'green' if cg.weight > 0 else 'red'
-#             width = str(0.1 + abs(cg.weight / 5.0))
-#             dot.edge(a, b, _attributes={'style': style, 'color': color, 'penwidth': width})
+    for cg in genome.connections.values():
+        if cg.enabled or show_disabled:
+            #if cg.input not in used_nodes or cg.output not in used_nodes:
+            #    continue
+            input, output = cg.key
+            a = node_names.get(input, str(input))
+            b = node_names.get(output, str(output))
+            style = 'solid' if cg.enabled else 'dotted'
+            color = 'green' if cg.weight > 0 else 'red'
+            width = str(0.1 + abs(cg.weight / 5.0))
+            dot.edge(a, b, _attributes={'style': style, 'color': color, 'penwidth': width})
 
-#     dot.render(filename, view=view)
+    dot.render(filename, view=view)
 
-#     return dot
+    return dot
