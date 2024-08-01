19a84fa7dd5ed7c9f4a2fa430e0785e755dbd0f1
VadymMakohon <errorks7@gmail.com>
2024-08-01 12:25:01 +0200

    revires code to previous statement as AI could not pass the more than 7 tubes

--- visualize.py
+++ visualize.py
@@ -7,24 +7,10 @@ import graphviz
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def check_matplotlib():
+def plot_stats(statistics, ylog=False, view=False, filename='avg_fitness.svg'):
+    """ Plots the population's average and best fitness. """
     if plt is None:
         warnings.warn("This display is not available due to a missing optional dependency (matplotlib)")
-        return False
-    return True
-
-
-def check_graphviz():
-    if graphviz is None:
-        warnings.warn("This display is not available due to a missing optional dependency (graphviz)")
-        return False
-    return True
-
-
-def plot_stats(statistics, ylog=False, view=False, filename='avg_fitness.svg'):
-    """Plots the population's average and best fitness."""
-    if not check_matplotlib():
         return
 
     generation = range(len(statistics.most_fit_genomes))
@@ -50,52 +36,52 @@ def plot_stats(statistics, ylog=False, view=False, filename='avg_fitness.svg'):
         plt.show()
     plt.close()
 
-
 def plot_spikes(spikes, view=False, filename=None, title=None):
-    """Plots the trains for a single spiking neuron."""
-    if not check_matplotlib():
-        return
-
+    """ Plots the trains for a single spiking neuron. """
     t_values = [t for t, I, v, u, f in spikes]
     v_values = [v for t, I, v, u, f in spikes]
     u_values = [u for t, I, v, u, f in spikes]
     I_values = [I for t, I, v, u, f in spikes]
     f_values = [f for t, I, v, u, f in spikes]
 
-    fig, axs = plt.subplots(4, 1, figsize=(10, 8))
-    fig.suptitle("Izhikevich's spiking neuron model" if title is None else f"Izhikevich's spiking neuron model ({title})")
+    fig = plt.figure()
+    plt.subplot(4, 1, 1)
+    plt.ylabel("Potential (mv)")
+    plt.xlabel("Time (in ms)")
+    plt.grid()
+    plt.plot(t_values, v_values, "g-")
+    plt.title("Izhikevich's spiking neuron model" if title is None else f"Izhikevich's spiking neuron model ({title})")
+    
+    plt.subplot(4, 1, 2)
+    plt.ylabel("Fired")
+    plt.xlabel("Time (in ms)")
+    plt.grid()
+    plt.plot(t_values, f_values, "r-")
 
-    axs[0].plot(t_values, v_values, "g-")
-    axs[0].set_ylabel("Potential (mv)")
-    axs[0].set_xlabel("Time (in ms)")
-    axs[0].grid()
+    plt.subplot(4, 1, 3)
+    plt.ylabel("Recovery (u)")
+    plt.xlabel("Time (in ms)")
+    plt.grid()
+    plt.plot(t_values, u_values, "r-")
 
-    axs[1].plot(t_values, f_values, "r-")
-    axs[1].set_ylabel("Fired")
-    axs[1].set_xlabel("Time (in ms)")
-    axs[1].grid()
-
-    axs[2].plot(t_values, u_values, "r-")
-    axs[2].set_ylabel("Recovery (u)")
-    axs[2].set_xlabel("Time (in ms)")
-    axs[2].grid()
-
-    axs[3].plot(t_values, I_values, "r-o")
-    axs[3].set_ylabel("Current (I)")
-    axs[3].set_xlabel("Time (in ms)")
-    axs[3].grid()
+    plt.subplot(4, 1, 4)
+    plt.ylabel("Current (I)")
+    plt.xlabel("Time (in ms)")
+    plt.grid()
+    plt.plot(t_values, I_values, "r-o")
 
     if filename is not None:
         plt.savefig(filename)
     if view:
         plt.show()
         plt.close()
+        fig = None
     return fig
 
-
 def plot_species(statistics, view=False, filename='speciation.svg'):
-    """Visualizes speciation throughout evolution."""
-    if not check_matplotlib():
+    """ Visualizes speciation throughout evolution. """
+    if plt is None:
+        warnings.warn("This display is not available due to a missing optional dependency (matplotlib)")
         return
 
     species_sizes = statistics.get_species_sizes()
@@ -114,45 +100,54 @@ def plot_species(statistics, view=False, filename='speciation.svg'):
         plt.show()
     plt.close()
 
-
 def draw_net(config, genome, view=False, filename=None, node_names=None, show_disabled=True, prune_unused=False,
              node_colors=None, fmt='svg'):
-    """Draws a neural network with arbitrary topology."""
-    if not check_graphviz():
+    """ Receives a genome and draws a neural network with arbitrary topology. """
+    # Attributes for network nodes.
+    if graphviz is None:
+        warnings.warn("This display is not available due to a missing optional dependency (graphviz)")
         return
 
     if node_names is None:
         node_names = {}
+
+    assert type(node_names) is dict
+
     if node_colors is None:
         node_colors = {}
+
+    assert type(node_colors) is dict
 
     node_attrs = {
         'shape': 'circle',
         'fontsize': '9',
         'height': '0.2',
-        'width': '0.2'
-    }
+        'width': '0.2'}
 
     dot = graphviz.Digraph(format=fmt, node_attr=node_attrs)
 
-    inputs = set(config.genome_config.input_keys)
-    outputs = set(config.genome_config.output_keys)
-
-    for k in inputs:
+    inputs = set()
+    for k in config.genome_config.input_keys:
+        inputs.add(k)
         name = node_names.get(k, str(k))
         input_attrs = {'style': 'filled', 'shape': 'box', 'fillcolor': node_colors.get(k, 'lightgray')}
         dot.node(name, _attributes=input_attrs)
 
-    for k in outputs:
+    outputs = set()
+    for k in config.genome_config.output_keys:
+        outputs.add(k)
         name = node_names.get(k, str(k))
-        output_attrs = {'style': 'filled', 'fillcolor': node_colors.get(k, 'lightblue')}
-        dot.node(name, _attributes=output_attrs)
+        node_attrs = {'style': 'filled', 'fillcolor': node_colors.get(k, 'lightblue')}
+        dot.node(name, _attributes=node_attrs)
 
-    used_nodes = set(genome.nodes.keys())
     if prune_unused:
-        connections = {cg.key for cg in genome.connections.values() if cg.enabled or show_disabled}
-        used_nodes = outputs.copy()
-        pending = outputs.copy()
+        connections = set()
+        for cg in genome.connections.values():
+            if cg.enabled or show_disabled:
+                connections.add((cg.in_node_id, cg.out_node_id))
+
+        used_nodes = copy.copy(outputs)
+        pending = copy.copy(outputs)
         while pending:
             new_pending = set()
             for a, b in connections:
@@ -160,14 +155,19 @@ def draw_net(config, genome, view=False, filename=None, node_names=None, show_di
                     new_pending.add(a)
                     used_nodes.add(a)
             pending = new_pending
+    else:
+        used_nodes = set(genome.nodes.keys())
 
     for n in used_nodes:
-        if n not in inputs and n not in outputs:
-            attrs = {'style': 'filled', 'fillcolor': node_colors.get(n, 'white')}
-            dot.node(str(n), _attributes=attrs)
+        if n in inputs or n in outputs:
+            continue
+        attrs = {'style': 'filled', 'fillcolor': node_colors.get(n, 'white')}
+        dot.node(str(n), _attributes=attrs)
 
     for cg in genome.connections.values():
         if cg.enabled or show_disabled:
+            #if cg.input not in used_nodes or cg.output not in used_nodes:
+            #    continue
             input, output = cg.key
             a = node_names.get(input, str(input))
             b = node_names.get(output, str(output))
@@ -177,4 +177,5 @@ def draw_net(config, genome, view=False, filename=None, node_names=None, show_di
             dot.edge(a, b, _attributes={'style': style, 'color': color, 'penwidth': width})
 
     dot.render(filename, view=view)
+
     return dot
